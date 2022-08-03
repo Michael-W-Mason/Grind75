9427e8e5219f9d361cb85d52685316f7822456a5
Michael Mason <mwmason1998@gmail.com>
2022-08-03 09:36:16 -0400

    twoSum Completed

--- twosum.js
+++ twosum.js
@@ -5,12 +5,14 @@
  */
 var twoSum = function (nums, target) {
     let hash = {};
-    for(let i =0; i < nums; i++){
-        if(!hash[i]){
-            hash[target - nums[i]] = i;
+    for (let i = 0; i < nums.length; i++) {
+        if (hash[nums[i]] !== undefined) {
+            return [hash[nums[i]], i];
         }
-        else if(){
-
+        else {
+            hash[target - nums[i]] = i;
         }
     }
 };
+
+console.log(twoSum([2, 7, 11, 15], 9));
